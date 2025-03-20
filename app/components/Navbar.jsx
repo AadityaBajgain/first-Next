90f83c2c2cdfde9fbfa993798aed193bef40749e
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-03-20 15:05:23 -0700

    Add initial layout and components for HelpDesk app with Tailwind CSS integration

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+export default function Navbar() {
+  return (
+    <nav className="pb-4
+    border-b-2 border-gray-200
+    flex items-center gap-5 
+    my-10 mx-auto 
+    max-w-5xl;">
+      <h1 className="text-[var(--primary)]">HelpDesk</h1>
+      <Link href = '/' className="hover:text-black">Dashboard</Link>
+      <Link href = '/tickets' className="hover:text-black">Tickets</Link>
+    </nav>
+  )
+}
